2d71f8f93f5faaaf9557a5c22e46fc4aeb11f3f8
Евгений <evsadovsky@yandex.ru>
2016-09-22 15:03:53 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@
 SCRIPTS_PATH=/mnt/install-scripts
 
 apt-get update
-apt-get install git
 
 git clone --depth 1 https://github.com/superdesk/install-scripts.git $SCRIPTS_PATH
 git clone --depth 1 https://github.com/superdesk/superdesk.git /mnt/superdesk
